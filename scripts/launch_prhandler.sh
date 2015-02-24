a38c50a1b1bd82ed4dbc365e5d0899457791cfa5
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-02-23 18:41:44 -0600

    Add prhanler's launcher

---
+++ scripts/launch_prhandler.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NAME=prhandler
+IMAGE=hub.yt/prhandler:yt
+
+docker pull $IMAGE
+( docker rm -f $NAME > /dev/null ; true )
+docker run \
+   -d \
+   -p 8888 \
+   --name $NAME $IMAGE

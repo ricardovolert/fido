9477d177a7755dd4086c65634aad76d42a2ae80a
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-03-09 08:27:09 -0500

    Update tester

--- tester/yt_test.py
+++ tester/yt_test.py
@@ -14,11 +14,13 @@ DEFAULT_IMAGE = "%s/%s:%s" % (IMAGE_REPO, IMAGE_NAME, IMAGE_TAG)
 DEFAULT_COMMAND = "nosetests -v units/tests"
 DEFAULT_OUTPUT = "unittests.xml"
 
+
 def run_test(image=DEFAULT_IMAGE,
              command="nosetests -v units/tests",
-             output="unittests.xml"):
+             output="unittests.xml",
+             build_docs=False):
     print("Using image: %s" % image)
-    dcli = docker.Client(base_url=BASE_URL)
+    dcli = docker.Client(base_url=BASE_URL, timeout=360)
     contid = dcli.create_container(
         image=image,
         command=command,
@@ -26,18 +28,25 @@ def run_test(image=DEFAULT_IMAGE,
     )
     dcli.start(contid,
                binds={"/mnt/data/volumes/yt_data/": {"bind": "/mnt/yt/"}})
-    for line in dcli.logs(contid, stdout=True, stderr=True,
-                          stream=True, timestamps=False):
-        print(line.strip())
 
     retcode = dcli.wait(contid)
+    print dcli.logs(contid, stdout=True, stderr=True,
+                    stream=False, timestamps=False)
+
     try:
         fo = StringIO(dcli.copy(contid, "/tmp/yt/nosetests.xml").read())
         tar = tarfile.open(mode="r", fileobj=fo)
         with open(output, 'w') as fh:
             fh.write(tar.extractfile('nosetests.xml').read())
     except docker.errors.APIError as e:
-        print("nosetests.xml not found :(")
+        print("nosetests.xml not found :( -> %s" % e)
+
+    if build_docs:
+        try:
+            with open("docs.tar", "w") as fh:
+                fh.write(dcli.copy(contid, "/tmp/yt/doc/build/html/").read())
+        except docker.errors.APIError as e:
+            print("extracting docs failed :( %s" % e)
 
     dcli.remove_container(contid, force=True)
     os._exit(retcode)

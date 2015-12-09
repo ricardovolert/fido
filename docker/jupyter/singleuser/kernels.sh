db269030d862b3d290d23a0d310bfb0c33ed530d
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-09 15:03:05 -0600

    jupyter/singleuser: add missing ipykernel dep, fix python2 install

--- docker/jupyter/singleuser/kernels.sh
+++ docker/jupyter/singleuser/kernels.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 KERNDIR=/usr/local/share/jupyter/kernels
+UKERNDIR=$HOME/.local/share/jupyter/kernels/
+
+mkdir -p $UKERNDIR
 
 for pyver in 2 3; do
    for branch in dev stable; do
-       sudo -H -u jovyan bash -c "/opt/conda/bin/conda env create -n py${pyver}-${branch} python=${pyver} -f /tmp/yt${branch}.yaml"
-       cp -r ${KERNDIR}/python2 $KERNDIR/py${pyver}-${branch}
-       sed -i ${KERNDIR}/py${pyver}-${branch}/kernel.json \
+       sed -e "/dependencies/ a- python=${pyver}" /tmp/yt${branch}.yaml > /tmp/${pyver}yt${branch}.yaml
+       /opt/conda/bin/conda env create -n py${pyver}-${branch} -f /tmp/${pyver}yt${branch}.yaml
+       cp -r ${KERNDIR}/python2 $UKERNDIR/py${pyver}-${branch}
+       sed -i ${UKERNDIR}/py${pyver}-${branch}/kernel.json \
           -e "/display_name/ s/Python 2/yt (${branch}) - py${pyver}/" \
           -e "/conda/ s/python2/py${pyver}-${branch}/"
     done
 done
-
-rm -rf ${KERNDIR}/python2

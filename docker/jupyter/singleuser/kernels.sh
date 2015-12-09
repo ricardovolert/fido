e8ac948cb6cfe4fb4c1bc5fbb8271dde5752be22
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-09 14:09:58 -0600

    jupyter/singleuser: Add more kernels

---
+++ docker/jupyter/singleuser/kernels.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+KERNDIR=/usr/local/share/jupyter/kernels
+
+for pyver in 2 3; do
+   for branch in dev stable; do
+       sudo -H -u jovyan conda env create -n py${pyver}-${branch} python=${pyver} -f /tmp/yt${branch}.yaml
+       cp -r ${KERNDIR}/python2 $KERNDIR/py${pyver}-${branch}
+       sed -i ${KERNDIR}/py${pyver}-${branch}/kernel.json \
+          -e "/display_name/ s/Python 2/yt (${branch}) - py${pyver}/" \
+          -e "/conda/ s/python2/py${pyver}-${branch}/"
+    done
+done
+
+rm -rf ${KERNDIR}/python2

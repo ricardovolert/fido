84020dbb72ad6816ab8328b76252886ef9caeefe
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-09 14:24:07 -0600

    jupyter/singleuser: fix path

--- docker/jupyter/singleuser/kernels.sh
+++ docker/jupyter/singleuser/kernels.sh
@@ -3,7 +3,7 @@ KERNDIR=/usr/local/share/jupyter/kernels
 
 for pyver in 2 3; do
    for branch in dev stable; do
-       sudo -H -u jovyan conda env create -n py${pyver}-${branch} python=${pyver} -f /tmp/yt${branch}.yaml
+       sudo -H -u jovyan bash -c "/opt/conda/bin/conda env create -n py${pyver}-${branch} python=${pyver} -f /tmp/yt${branch}.yaml"
        cp -r ${KERNDIR}/python2 $KERNDIR/py${pyver}-${branch}
        sed -i ${KERNDIR}/py${pyver}-${branch}/kernel.json \
           -e "/display_name/ s/Python 2/yt (${branch}) - py${pyver}/" \

99b229e49dc887f33d15312bc1cc6c62938b089c
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-10 13:26:53 -0600

    jupyter/singleuser: fix permissions, provide PATH

--- docker/jupyter/singleuser/singleuser.sh
+++ docker/jupyter/singleuser/singleuser.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 usermod -u $LDAP_UID jovyan
+chown -R jovyan:users /home/jovyan
 sudo -H -n -u jovyan bash -c "( echo \"$JPY_USER\n$TOTP_TOKEN\nY\n\" | mount /home/jovyan/work )"
 sudo -H -n -u jovyan \
-   bash -c "jupyterhub-singleuser \
+   PATH=/opt/conda/bin:$PATH jupyterhub-singleuser \
       --port=8888 \
       --ip=0.0.0.0 \
       --user=$JPY_USER \
       --cookie-name=$JPY_COOKIE_NAME \
       --base-url=$JPY_BASE_URL \
       --hub-prefix=$JPY_HUB_PREFIX \
-      --hub-api-url=$JPY_HUB_API_URL"
+      --hub-api-url=$JPY_HUB_API_URL

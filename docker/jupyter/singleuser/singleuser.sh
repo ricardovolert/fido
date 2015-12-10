75f6f85decb9b1158768eb55c0551657a219b4fe
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-10 15:42:08 -0600

    jupyter/singleuser: simplify davfs mounting, fix env

--- docker/jupyter/singleuser/singleuser.sh
+++ docker/jupyter/singleuser/singleuser.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 usermod -u $LDAP_UID jovyan
+echo "/home/jovyan/work ${JPY_USER} ${TOTP_TOKEN}" > /home/jovyan/.davfs2/secrets
 chown -R jovyan:users /home/jovyan
-sudo -H -n -u jovyan bash -c "( echo \"$JPY_USER\n$TOTP_TOKEN\nY\n\" | mount /home/jovyan/work )"
-sudo -H -n -u jovyan \
+sudo -E -H -n -u jovyan mount /home/jovyan/work
+sudo -E -H -n -u jovyan \
    PATH=/opt/conda/bin:$PATH jupyterhub-singleuser \
       --port=8888 \
       --ip=0.0.0.0 \

c142d8dbdd5cd4877b94297f318fe19054574135
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-10 12:50:20 -0600

    jupyter/singleuser: Use ldap uid for jovyan

--- docker/jupyter/singleuser/singleuser.sh
+++ docker/jupyter/singleuser/singleuser.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-( echo "$JPY_USER\n$TOTP_TOKEN\nY\n" | mount /home/jovyan/work )
-
-jupyterhub-singleuser \
-  --port=8888 \
-  --ip=0.0.0.0 \
-  --user=$JPY_USER \
-  --cookie-name=$JPY_COOKIE_NAME \
-  --base-url=$JPY_BASE_URL \
-  --hub-prefix=$JPY_HUB_PREFIX \
-  --hub-api-url=$JPY_HUB_API_URL
+usermod -u $LDAP_UID jovyan
+sudo -H -n -u jovyan bash -c "( echo \"$JPY_USER\n$TOTP_TOKEN\nY\n\" | mount /home/jovyan/work )"
+sudo -H -n -u jovyan \
+   bash -c "jupyterhub-singleuser \
+      --port=8888 \
+      --ip=0.0.0.0 \
+      --user=$JPY_USER \
+      --cookie-name=$JPY_COOKIE_NAME \
+      --base-url=$JPY_BASE_URL \
+      --hub-prefix=$JPY_HUB_PREFIX \
+      --hub-api-url=$JPY_HUB_API_URL"

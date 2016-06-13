b67bb72649531281ff91d9159a27fb342e921a0c
Kacper Kowalik <xarthisius.kk@gmail.com>
2016-06-13 15:07:58 -0500

    Update notebook image, add branding

--- docker/jupyter/singleanonuser/kernels.sh
+++ docker/jupyter/singleanonuser/kernels.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-KERNDIR=/usr/local/share/jupyter/kernels
+KERNDIR=/opt/conda/share/jupyter/kernels/python3
 UKERNDIR=$HOME/.local/share/jupyter/kernels/
 
 mkdir -p $UKERNDIR
@@ -8,10 +8,10 @@ for pyver in 2 3; do
    for branch in dev stable; do
        sed -e "/dependencies/ a- python=${pyver}" /tmp/yt${branch}.yaml > /tmp/${pyver}yt${branch}.yaml
        /opt/conda/bin/conda env create -q -n py${pyver}-${branch} -f /tmp/${pyver}yt${branch}.yaml
-       cp -r ${KERNDIR}/python2 $UKERNDIR/py${pyver}-${branch}
+       cp -r ${KERNDIR} $UKERNDIR/py${pyver}-${branch}
        sed -i ${UKERNDIR}/py${pyver}-${branch}/kernel.json \
-          -e "/display_name/ s/Python 2/yt (${branch}) - py${pyver}/" \
-          -e "/conda/ s/python2/py${pyver}-${branch}/"
+          -e "/display_name/ s/Python 3/yt (${branch}) - py${pyver}/" \
+          -e "/conda/ s:/opt/conda:&/envs/py${pyver}-${branch}:"
     done
 done
 

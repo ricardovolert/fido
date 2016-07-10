0542f2af64c04d48f3548931009a6d1f0d70eb16
Kacper Kowalik <xarthisius.kk@gmail.com>
2016-07-10 10:11:41 -0500

    [singleanonuser] add astropy and fix kernel.sh script

--- docker/jupyter/singleanonuser/kernels.sh
+++ docker/jupyter/singleanonuser/kernels.sh
@@ -1,17 +1,13 @@
-#!/bin/sh
-KERNDIR=/opt/conda/share/jupyter/kernels/python3
-UKERNDIR=$HOME/.local/share/jupyter/kernels/
-
-mkdir -p $UKERNDIR
+#!/bin/bash
+export PATH=/opt/conda/bin:$PATH
+export CONDA_ENVS=/opt/conda/envs
 
 for pyver in 2 3; do
    for branch in dev stable; do
        sed -e "/dependencies/ a- python=${pyver}" /tmp/yt${branch}.yaml > /tmp/${pyver}yt${branch}.yaml
-       /opt/conda/bin/conda env create -q -n py${pyver}-${branch} -f /tmp/${pyver}yt${branch}.yaml
-       cp -r ${KERNDIR} $UKERNDIR/py${pyver}-${branch}
-       sed -i ${UKERNDIR}/py${pyver}-${branch}/kernel.json \
-          -e "/display_name/ s/Python 3/yt (${branch}) - py${pyver}/" \
-          -e "/conda/ s:/opt/conda:&/envs/py${pyver}-${branch}:"
+       conda env create -q -p $CONDA_ENVS/py${pyver}-${branch} -f=/tmp/${pyver}yt${branch}.yaml
+       source activate $CONDA_ENVS/py${pyver}-${branch}
+       python -m ipykernel install --user --name "py${pyver}-${branch}" --display-name "py${pyver} (${branch})"
     done
 done
 
